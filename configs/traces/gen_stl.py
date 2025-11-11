ee56780e4d85eec4d8ef5b88e59e892b8eaba628
Gang.jia <gang.jia@transcputing.com>
2025-11-11 13:46:10 +0800

    3ddram-DL2T2.json also works. Next: Add random access trace files.wq

--- configs/traces/gen_stl.py
+++ configs/traces/gen_stl.py
@@ -1,7 +1,7 @@
-with open('512_write_29bit.stl', 'w') as file:
+with open('512_write_27bit.stl', 'w') as file:
     addr = 0;
     index = 0;
-    while (addr < 0x20000000):
+    while (addr < 0x8000000):
         print('%d: (512) write 0x%x' % (index,addr), file = file);
         addr += 0x200;
         index += 1;
