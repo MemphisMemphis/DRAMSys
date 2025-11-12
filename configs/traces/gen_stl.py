d5a3257ad6888e02b94ed9f7207a8577129ebf63
Gang.jia <gang.jia@transcputing.com>
2025-11-12 11:45:19 +0800

    gen_stl.py for random address trace file. run simulation by random address.

--- configs/traces/gen_stl.py
+++ configs/traces/gen_stl.py
@@ -1,9 +1,42 @@
-with open('512_write_27bit.stl', 'w') as file:
+import random
+
+burst_len = 512
+max_address_bit = 29
+trace_file_name = str(burst_len) + '_write_' + str(max_address_bit) + 'bits.stl'
+random_file_name = str(burst_len) + '_random_' + str(max_address_bit) + 'bits.stl'
+max_address = 1 << max_address_bit
+
+with open(trace_file_name, 'w') as trace_file:
     addr = 0;
     index = 0;
-    while (addr < 0x8000000):
-        print('%d: (512) write 0x%x' % (index,addr), file = file);
-        addr += 0x200;
+    while (index < max_address/burst_len):
+        print('%d: (%d) write 0x%x' % (index, burst_len, addr), file = trace_file);
+        addr += burst_len;
         index += 1;
 
-file.close();
+trace_file.close();
+
+
+# random address generation
+random_seed = 0
+
+# using the same seed multiple times
+random_block_bit = 27
+random_max_address = 1 << random_block_bit
+
+
+
+with open(random_file_name, 'w') as random_file:
+    index = 0
+    block_index = 0
+    for block_index in range(max_address//random_max_address):
+        random.seed(random_seed)
+        random_index = 0
+        while (random_index < random_max_address/burst_len):
+            addr = random.randint(0, random_max_address/burst_len) * burst_len + block_index * random_max_address
+            print('%d: (%d) write 0x%x' % (index, burst_len, addr), file = random_file)
+            random_index += 1
+            index += 1
+        
+    
+random_file.close();
