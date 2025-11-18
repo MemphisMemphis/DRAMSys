6e56cf414ef96cec1a858eefffa669e79f288932
Gang.jia <gang.jia@transcputing.com>
2025-11-18 17:28:19 +0800

    Add 3d-c1rb1d1-DL2T2.json: IN: AXI 128Bx800MHz = 256Bx400MHz, Burst length = 4; OUT: MC 32Bx400MHz x 8, Burst length = 4; DL2T2: 8 MC (4 IP1) x 4 AXI x 4 controller; DL4T2: Burst length = 8 addition 2 cycles; DL4T1: 8 MC x 4 AXI x 2 controller, Burst length = 16 + 2x3 cycles; As no DEVICE_BIT, device=1 for 8 MC.

--- configs/traces/gen_stl.py
+++ configs/traces/gen_stl.py
@@ -1,8 +1,8 @@
 import random
 
-burst_len = 512*32
-max_address_bit = 30
-trace_file_name = str(burst_len) + '_write_' + str(max_address_bit) + 'bits.stl'
+burst_len = 32*4
+max_address_bit = 25
+trace_file_name = str(burst_len) + '_read_' + str(max_address_bit) + 'bits.stl'
 random_file_name = str(burst_len) + '_random_' + str(max_address_bit) + 'bits.stl'
 max_address = 1 << max_address_bit
 
@@ -10,7 +10,7 @@ with open(trace_file_name, 'w') as trace_file:
     addr = 0;
     index = 0;
     while (index < max_address/burst_len):
-        print('%d: (%d) write 0x%x' % (index, burst_len, addr), file = trace_file);
+        print('%d: (%d) read 0x%x' % (index, burst_len, addr), file = trace_file);
         addr += burst_len;
         index += 1;
 
@@ -21,7 +21,7 @@ trace_file.close();
 random_seed = 0
 
 # using the same seed multiple times
-random_block_bit = 30
+random_block_bit = 25
 random_max_address = 1 << random_block_bit
 
 
@@ -34,7 +34,7 @@ with open(random_file_name, 'w') as random_file:
         random_index = 0
         while (random_index < random_max_address/burst_len):
             addr = random.randint(0, random_max_address/burst_len) * burst_len + block_index * random_max_address
-            print('%d: (%d) write 0x%x' % (index, burst_len, addr), file = random_file)
+            print('%d: (%d) read 0x%x' % (index, burst_len, addr), file = random_file)
             random_index += 1
             index += 1
         
